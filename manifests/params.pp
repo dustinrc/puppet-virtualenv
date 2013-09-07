40265e89d1603c3eaa5791725f967c0409dc67cf
Dustin Chapman <dustin.r.chapman@gmail.com>
2013-09-06 23:54:42 -0400

    Gentoo supported

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,8 +6,14 @@ class virtualenv::params {
     'Debian': {
       $package_name    = 'python-virtualenv'
     }
+    'Gentoo': {
+      $package_name    = 'dev-python/virtualenv'
+    }
     'RedHat': {
       $package_name    = 'python-virtualenv'
+    }
+    default: {
+      fail("The ${module_name} module is not supported on an ${::osfamily} based system.")
     }
   }
 
